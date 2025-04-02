06605b68a5b01fdf7d57b031ada6cd893d727ad6
Victor Maciel <victorgmaciel@protonmail.com>
2025-04-02 15:45:02 -0700

    practice problem work

---
+++ SSML/practice_problem.py
@@ -0,0 +1,31 @@
+"""
+Practice Problem (Mock)
+"""
+
+
+
+test_case_one = 58
+
+def roman_numeral_creator(num: int) -> str:
+
+    roman_numeral = {
+        "M": 1000,
+        "D": 500,
+        "C": 100,
+        "L": 50,
+        "X": 10,
+        "V": 5,
+        "I": 1,
+    }
+
+    roman_result = ""
+    for key, value in roman_numeral.items():
+            while num >= value:
+                roman_result += key
+                num -= value
+    print(roman_result)
+
+
+
+roman_numeral_creator(test_case_one)
+
