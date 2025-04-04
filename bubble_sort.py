b3e7b278b81d735437e04d254dc0351ccbd1a897
Victor Maciel <victorgmaciel@protonmail.com>
2025-04-04 12:01:25 -0700

    never use

---
+++ bubble_sort.py
@@ -0,0 +1,15 @@
+def bubble_sort(nums):
+    swapping = True
+
+    end = len(nums)
+
+    while swapping is True:
+        swapping = False
+        for i in range(1, end):
+            if nums[i - 1] > nums[i]:
+                temp = nums[i - 1]
+                nums[i - 1] = nums[i]
+                nums[i] = temp
+                swapping = True
+        end -= 1
+    return nums
