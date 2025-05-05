aa3a06b79395e114d086b37dd613f1c3ada2fd8d
Victor Maciel <victorgmaciel@protonmail.com>
2025-05-05 10:20:56 -0700

    adding

--- boot_dev/loops/loops.py
+++ boot_dev/loops/loops.py
@@ -12,3 +12,15 @@ def regenerate(current_health, max_health, enemy_distance):
         current_health += 1
         enemy_distance -= 2
     return current_health
+
+
+
+
+
+def countdown_to_start():
+
+    for i in range(11,0, -1):
+        print(i)
+
+countdown_to_start()
+
